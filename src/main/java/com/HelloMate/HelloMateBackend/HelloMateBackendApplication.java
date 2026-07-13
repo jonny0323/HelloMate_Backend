0db756216b96ddf550875c664662f5b616651388
TaeJoon Lee <taejoonlee@TaeJoonui-noteubug.local>
2026-07-13 12:37:12 +0900

    first commit

---
+++ src/main/java/com/HelloMate/HelloMateBackend/HelloMateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.HelloMate.HelloMateBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloMateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HelloMate/HelloMateBackend/HelloMateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HelloMate.HelloMateBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
